0ed60a0306c2e44a0514113b903531923bf46d90
emin <398004517@qq.com>
2018-07-30 10:30:43 +0800

    update

--- src/test/java/com/junittest/demo/service/UserServiceTest.java
+++ src/test/java/com/junittest/demo/service/UserServiceTest.java
@@ -2,8 +2,12 @@ package com.junittest.demo.service;
 
 import com.junittest.demo.domin.User;
 import com.junittest.demo.mapper.UserMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,21 +19,27 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.when;
 
 //测试Service和测试Controller类似，同样采用隔离法
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceTest {
-    @MockBean
+    @Mock
     private UserMapper userMapper;
-    @Autowired
+    @InjectMocks
     private UserService userServiceImpl;
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+    }
     @Test
     public void testModelServiceServiceImpl(){
         User mockuser = new User();
         mockuser.setName("lisi");
-        given(userMapper.getUserById(anyInt()))
-                .willReturn(mockuser);
+        when(userMapper.getUserById(anyInt())).thenReturn(mockuser);
+//        given(userMapper.getUserById(anyInt()))
+//                .willReturn(mockuser);
         User user =  userServiceImpl.getUserById(1);
         assertThat(user.getName(),equalTo("lisi"));
     }
